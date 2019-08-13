5d2e61f31558a5560d08daaf1696e62c12f3211c
Th3Whit3Wolf <the.white.wolf.is.1337@gmail.com>
2019-08-13 14:09:17 +0100

    git subrepo clone https://github.com/Th3Whit3Wolf/pop-splash.git
    subrepo:
      subdir:   "pop-splash"
      merged:   "285ef0d205"
    upstream:
      origin:   "https://github.com/Th3Whit3Wolf/pop-splash.git"
      branch:   "master"
      commit:   "285ef0d205"
    git-subrepo:
      version:  "0.4.1"
      origin:   "???"
      commit:   "???"

---
+++ pop-splash/Pop/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -0,0 +1,176 @@
+var plasma = getApiVersion(1);
+
+var layout = {
+    "desktops": [
+        {
+            "applets": [
+            ],
+            "config": {
+                "/": {
+                    "formfactor": "0",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                },
+                "/ConfigDialog": {
+                    "DialogHeight": "420",
+                    "DialogWidth": "560"
+                },
+                "/General": {
+                    "ToolBoxButtonState": "left",
+                    "ToolBoxButtonY": "28",
+                    "pressToMoveHelp": "false",
+                    "showToolbox": "false"
+                },
+                "/Wallpaper/org.kde.image/General": {
+                    "Image": "file:///usr/share/backgrounds/System76-Unleash_Your_Robot_Blue-by_Kate_Hazen_of_System76.png"
+                }
+            },
+            "wallpaperPlugin": "org.kde.image"
+        }
+    ],
+    "panels": [
+        {
+            "alignment": "left",
+            "applets": [
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "PreloadWeight": "100"
+                        },
+                        "/Configuration/General": {
+                            "favoritesPortedToKAstats": "true",
+                            "systemApplications": "systemsettings.desktop,org.kde.kinfocenter.desktop"
+                        },
+                        "/Configuration/Shortcuts": {
+                            "global": "Alt+F1"
+                        },
+                        "/Shortcuts": {
+                            "global": "Alt+F1"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.kickoff"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration/ConfigDialog": {
+                            "DialogHeight": "420",
+                            "DialogWidth": "560"
+                        },
+                        "/Configuration/General": {
+                            "showOnlyCurrentScreen": "true",
+                            "showWindowIcons": "true"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.pager"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.icontasks"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration/ConfigDialog": {
+                            "DialogHeight": "420",
+                            "DialogWidth": "560"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.appmenu"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration/General": {
+                            "length": "670"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.panelspacer"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.digitalclock"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration/General": {
+                            "expanding": "false",
+                            "length": "747"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.panelspacer"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration": {
+                            "PreloadWeight": "55"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.systemtray"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "immutability": "1"
+                        },
+                        "/Configuration/ConfigDialog": {
+                            "DialogHeight": "420",
+                            "DialogWidth": "560"
+                        },
+                        "/Configuration/General": {
+                            "showName": "false",
+                            "showSett": "true"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.uswitcher"
+                }
+            ],
+            "config": {
+                "/": {
+                    "formfactor": "2",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                },
+                "/ConfigDialog": {
+                    "DialogHeight": "71",
+                    "DialogWidth": "1920"
+                }
+            },
+            "height": 1.5555555555555556,
+            "hiding": "normal",
+            "location": "top",
+            "maximumLength": 106.66666666666667,
+            "minimumLength": 106.66666666666667,
+            "offset": 0
+        }
+    ],
+    "serializationFormatVersion": "1"
+}
+;
+
+plasma.loadSerializedLayout(layout);
---
+++ pop-splash/README.md
@@ -0,0 +1,14 @@
+# Pop
+This is a KDE Look and Feel Theme(with a splash screen) to make KDE look and feel like Pop!_OS.
+
+### Screenshots
+#### Desktop
+![preview](Pop/contents/previews/fullscreenpreview.jpg)
+
+#### Splash
+![splash](Pop/contents/previews/splash.png)
+
+# Credits
+[Pop!_OS Plymouth](https://github.com/pop-os/plymouth-theme) is where I got the pop icon that fits so well.
+
+[Pop!_OS Gtk Theme](https://github.com/pop-os/gtk-theme) is where I got the colors from.
